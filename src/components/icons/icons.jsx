762ba42f6e001be3f325106b2f448e6fb3ae9a7e
Enrico Mani <manienrico13@gmail.com>
2025-01-13 20:32:34 +0300

    Socials structure done

---
+++ src/components/icons/icons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SocialIcons() {
+  return (
+    <ul>
+        <li><a href="">X</a></li>
+        <li><a href="">Hangouts</a></li>
+        <li><a href="">Facebook</a></li>
+        <li><a href="">Badoo</a></li>
+    </ul>
+  )
+}
