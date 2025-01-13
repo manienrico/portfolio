892c6ebe33ecc129789d6ce89dfae54a4f45a204
Enrico Mani <manienrico13@gmail.com>
2025-01-13 20:36:06 +0300

    CompanyInfo structured

---
+++ src/body/CompanyInfo/CompanyInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CompanyInfo() {
+  return (
+    <div>CompanyInfo
+        <h1>Role</h1>
+        <p>Field</p>
+        <p>company</p>
+    </div>
+  )
+}
