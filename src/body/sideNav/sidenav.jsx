04687a0b4e68e2f59f5c6ab3ca58963baad139bd
Enrico Mani <manienrico13@gmail.com>
2025-01-18 20:36:04 +0300

    side nav aligned

--- src/body/sideNav/sidenav.jsx
+++ src/body/sideNav/sidenav.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function SideNav() {
   return (
-    <ul>
-        <li><a href="#" className="text-white-500 hover:text-orange-600 font-semibold" >ABOUT</a></li>
-        <li><a href="#" className="text-white-500 hover:text-orange-600 font-semibold" >PROJECTS</a></li>
-        <li><a href="#" className="text-white-500 hover:text-orange-600 font-semibold" >CONTACT</a></li>
+    <div className="relative h-screen flex items-center justify-end">
+    <ul className="absolute right-0 top-1/1 transform -translate-y-1/2 space-y-4 " >
+        <li className=" "><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-orange-600 font-semibold" >ABOUT</a></li>
+        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-orange-600 font-semibold " >PROJECTS</a></li>
+        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-orange-600 font-semibold" >CONTACT</a></li>
     </ul>
+    </div>
   )
 }
