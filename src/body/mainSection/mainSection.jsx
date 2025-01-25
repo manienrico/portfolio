8fdc004d3155e87d279602087a439bf65dc5a0dc
Enrico Mani <manienrico13@gmail.com>
2025-01-25 20:36:29 +0300

    circle size adjusted

--- src/body/mainSection/mainSection.jsx
+++ src/body/mainSection/mainSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MainSection() {
   return (
     <section className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center h-screen z-10   '>
-      <div className='w-32 h-32 rounded-full border-[1.5rem] border-red-500'>
+      <div className='w-80 h-80 rounded-full border-[1rem] border-red-500'>
 
       </div>
     </section>
