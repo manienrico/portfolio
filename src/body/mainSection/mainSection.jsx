9599b023a3cd5da10dd5f567cdabeebe25d89c01
Enrico Mani <manienrico13@gmail.com>
2025-01-25 20:58:50 +0300

    circle n img positioning abit

--- src/body/mainSection/mainSection.jsx
+++ src/body/mainSection/mainSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import pic from "../../imgs/profile.png"
 
 export default function MainSection() {
   return (
     <section className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center h-screen z-10   '>
       <div className='w-80 h-80 rounded-full border-[1rem] border-red-500'>
-
+        <img src={pic} alt='Profile picture' className='rouded-full' />
       </div>
     </section>
   )
