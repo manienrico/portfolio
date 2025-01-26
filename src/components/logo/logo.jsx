50ae5e4e19a611595279c87c9017d2e46e923cba
Enrico Mani <manienrico13@gmail.com>
2025-01-26 19:24:30 +0300

    logo text colored

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logoPic from '../../imgs/anonyms.png'
 
 export default function Logo() {
   return (
-    <span>
-        <div>Logo Pic</div><div>Logo Text</div>
+    <span className='relative flex items-center'>
+        <div className='flex items-center'><img src={logoPic} alt='Anonyms Logo' className='w-32 h-auto rounded-full' /></div><div className='-ml-8 text-red-400 font-mediun'>ANONYMS</div>
     </span>
   )
 }
