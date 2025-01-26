c005fbebf66e1a9755d0d4a3f49f0cc610d1b34d
Enrico Mani <manienrico13@gmail.com>
2025-01-26 20:24:52 +0300

    side nav styling challenges but color successfuly changed

--- src/body/sideNav/sidenav.jsx
+++ src/body/sideNav/sidenav.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function SideNav() {
   return (
     <div className="relative h-screen flex items-center justify-end">
-    <ul className="absolute right-0 top-1/1 transform -translate-y-1/2 space-y-4 " >
-        <li className=" "><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-orange-600 font-semibold" >ABOUT</a></li>
-        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-orange-600 font-semibold " >PROJECTS</a></li>
-        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-orange-600 font-semibold" >CONTACT</a></li>
+    <ul className="absolute right-0 top-1/1 transform -translate-y-1/2 space-y-4" >
+        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-red-500 font-semibold" >ABOUT</a></li>
+        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-red-500 font-semibold " >PROJECTS</a></li>
+        <li className=""><a href="#" className="block [writing-mode:vertical-lr] text-white-500 hover:text-red-500 font-semibold" >CONTACT</a></li>
     </ul>
     </div>
   )
