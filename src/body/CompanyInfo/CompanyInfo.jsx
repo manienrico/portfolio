a2ba716adfdabe9d37a41e2d5f9f51408f7383e2
Enrico Mani <87425895+manienrico@users.noreply.github.com>
2025-01-31 07:47:14 +0000

    left side footer content added

--- src/body/CompanyInfo/CompanyInfo.jsx
+++ src/body/CompanyInfo/CompanyInfo.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default function CompanyInfo() {
   return (
-    <div className='flex'>CompanyInfo
-        <h1>Role</h1>
-        <p>Field</p>
-        <p>company</p>
+    <div className='flex flex-col'>
+        <h1>Web Designer</h1>
+        <div>
+          <span>From</span>
+          <span>IN</span>
+        </div>
+        
     </div>
   )
 }
--- src/body/Footer/Footer.jsx
+++ src/body/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import SocialIcons from '../../components/icons/icons'
 
 export default function Footer() {
   return (
-    <footer className='absolute bottom-10 w-full float-end bg-blue-500  '>
+    <footer className='absolute bottom-10 w-full float-end  '>
       <div className=' mx-auto flex flex-col md:flex-row justify-between '>
           <div className='float-left '><CompanyInfo /></div>
           <div className='float-right '><SocialIcons /></div>
